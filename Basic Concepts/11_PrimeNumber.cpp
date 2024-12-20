ce5ea59cabc455408886943fcec6a190b0c363a3
JavaidIqbal786 <87638239+JavaidIqbal786@users.noreply.github.com>
2024-12-20 17:21:06 +0500

    Find number is prime or not

--- Basic Concepts/11_PrimeNumber.cpp
+++ Basic Concepts/11_PrimeNumber.cpp
@@ -6,14 +6,14 @@ using namespace std;
 int main()
  {
     int i,n,count=0;
-    cout<<"Enter a number:";
+    cout<<"Enter a number:"<<endl;
     cin>>n;
     for(i=2;i<=n;i++)
         if(n%i==0)
             count=count+1;
     if(count==1)
-        cout<<"\nNumber is prime.";
+        cout<<n<<" is prime.";
     else
-        cout<<"\nNumber is not prime.";
+        cout<<n<<" is not prime.";
     return 0;
 }
